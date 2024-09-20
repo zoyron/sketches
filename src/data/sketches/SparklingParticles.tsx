9f1db0291cc15639092012588ac20695885d397a
zoyron <sgrarora3@gmail.com>
2024-09-20 11:40:43 +0530

    increased the number of particles and added more waves

--- src/data/sketches/SparklingParticles.tsx
+++ src/data/sketches/SparklingParticles.tsx
@@ -3,7 +3,7 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import * as THREE from "three";
 import { OrbitControls } from "@react-three/drei";
 
-const Particles = ({ count = 5000 }) => {
+const Particles = ({ count = 50000 }) => {
   const mesh = useRef<THREE.Points>(null!);
   const light = useRef<THREE.PointLight>(null!);
 
@@ -23,7 +23,8 @@ const Particles = ({ count = 5000 }) => {
     for (let i = 0; i < count; i++) {
       const i3 = i * 3;
       mesh.current.geometry.attributes.position.array[i3 + 1] =
-        Math.sin(time + particles[i3]) * 0.1;
+        Math.sin(time + particles[i3]) * 0.4 +
+        Math.cos(time + particles[i3]) * 0.4;
     }
     mesh.current.geometry.attributes.position.needsUpdate = true;
     light.current.position.set(
@@ -47,7 +48,7 @@ const Particles = ({ count = 5000 }) => {
         </bufferGeometry>
         <pointsMaterial
           size={0.02}
-          color="white"
+          color="#0080fa"
           transparent
           opacity={0.8}
           blending={THREE.AdditiveBlending}
