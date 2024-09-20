938954110b96278f70add3e1fd8a8711255ba75a
zoyron <sgrarora3@gmail.com>
2024-09-20 20:19:18 +0530

    took care of the types error

--- src/data/sketches/RotatingCube.tsx
+++ src/data/sketches/RotatingCube.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const RotatingCube: React.FC = () => {
   const mountRef = useRef<HTMLDivElement>(null);
