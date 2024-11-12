594343499ae2f7de2b4e527b1672b644f87006d4
zoyron <sgrarora3@gmail.com>
2024-11-12 22:20:37 +0530

    modified the welcome page

--- src/data/sketches/HomePage.tsx
+++ src/data/sketches/HomePage.tsx
@@ -3,15 +3,14 @@ import React from "react";
 const HomePage: React.FC = () => {
   return (
     <div className="w-full h-full bg-[#1a1a2e] text-gray-100 font-sans flex items-center justify-center">
-      <div className="w-full text-center">
-        <h1 className="text-7xl font-medium mb-8 text-white">
+      <div className="max-w-4xl w-full">
+        <h1 className="text-7xl font-medium m-8 text-white">
           Hello, I'm
-          <span className="block mt-4 text-indigo-400">Sagar</span>
+          <span className="block mt-4 text-[#7e5bef]">SAGAR</span>
         </h1>
-
-        <p className="text-3xl text-gray-400">
+        <p className="text-3xl m-8 text-gray-400">
           Welcome to my creative corner where I explore and experiment with
-          ThreeJS
+          <span className="mt-4 text-[#ff49db]"> ThreeJs</span>
         </p>
       </div>
     </div>
