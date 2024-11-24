aacd84adc491dafd3d82f5fce9c2d90412e8c2b0
zoyron <sgrarora3@gmail.com>
2024-11-24 23:48:05 +0530

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import {
   Routes,
   Navigate,
 } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 import GalleryPage from "./components/GalleryPage";
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-100 to-gray-200 text-gray-900">
+      <Analytics />
       <Router>
         <Routes>
           <Route path="/sketch/:sketchId" element={<GalleryPage />} />
